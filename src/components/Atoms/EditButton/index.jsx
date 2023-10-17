04bceedab8199c5726730a363bd850060dbf2579
toma0705 <t.fukuoka.23.6.1075@4nonome.com>
2023-10-17 18:58:24 +0900

    修正

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -6,14 +6,14 @@ import styled from "styled-components";
 const SampleButton = ({ onClick }) => {
   return (
     <StyledButton onClick={onClick}>
-      <StyledRound></StyledRound>
+      <StyledRound />
       <img
         style={{
           position: "absolute",
-          top: 2.5,
-          left: 2.5,
-          width: 15,
-          height: 15,
+          top: "0 px",
+          left: "0 px",
+          width: "20 px",
+          height: "20 px",
         }}
         src={pencil}
       />
@@ -25,9 +25,8 @@ const StyledRound = styled.div`
   border-radius: 50%;
   position: absolute;
   background-color: ${COLOR.LIGHT_GRAY_TRANSLUCENT};
-  z-index: 10;
-  top: 0;
-  left: 0;
+  top: 0px;
+  left: 0px;
   width: 20px;
   height: 20px;
   display: none;
@@ -38,6 +37,7 @@ const StyledButton = styled.button`
   position: relative;
   background: none;
   border: none;
+  padding: 0;
   width: 20px;
   height: 20px;
   &:hover ${StyledRound} {
