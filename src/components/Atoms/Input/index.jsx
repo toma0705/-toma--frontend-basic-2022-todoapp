1e293754971d3c70231c66133fb81413b0419472
toma0705 <t.fukuoka.23.6.1075@4nonome.com>
2023-10-24 10:42:10 +0900

    Atoms/Input

---
+++ src/components/Atoms/Input/index.jsx
@@ -0,0 +1,46 @@
+import React, { useRef, useEffect } from "react";
+import styled from "styled-components";
+import COLOR from "../../../variables/color.js";
+import FONTFAMILY from "../../../variables/font_family.js";
+import TEXT from "../../../variables/texts";
+
+export const Input = ({ onEditComplete, defaultValue }) => {
+  const inputRef = useRef(null);
+
+  const onPressEnter = (e) => {
+    if (e.key === "Enter") {
+      inputRef.current.blur();
+      inputRef.current.removeEventListener("blur", () => {
+        onEditComplete(inputRef.current.value);
+      });
+    }
+  };
+
+  useEffect(() => {
+    inputRef.current.value = defaultValue;
+    inputRef.current.focus();
+    inputRef.current.addEventListener("blur", () => {
+      onEditComplete(inputRef.current.value);
+    });
+    inputRef.current.addEventListener("keydown", (e) => onPressEnter(e));
+  }, []);
+
+  return <StyledInput ref={inputRef} type="text" />;
+};
+
+export default Input;
+
+const StyledInput = styled.input`
+  font-family: ${FONTFAMILY.NOTO_SANS};
+  ${TEXT.S}
+  color: ${COLOR.LIGHT_GRAY};
+  background-color: ${COLOR.BLACK};
+  border: none;
+  border-radius: 2px;
+  padding: 0px 4px;
+  width: 100%;
+  height: 20px;
+  &:focus {
+    outline: none;
+  }
+`;
